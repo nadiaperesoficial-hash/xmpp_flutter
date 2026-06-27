d26f3228461bd89e20ea88e6ecfda54cc4ec3e20
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-06-27 20:58:41 -0300

    Update chats_repo.dart

--- lib/repo/chats_repo.dart
+++ lib/repo/chats_repo.dart
@@ -6,7 +6,6 @@ import 'package:simple_chat/repo/db/db.dart';
 import 'package:simple_chat/repo/db/db_chat.dart';
 import 'package:simple_chat/repo/ui_chat.dart';
 import 'package:simple_chat/service_locator/service_locator.dart';
-import 'package:xmpp_plugin/xmpp_plugin.dart';
 
 abstract class ChatsRepo {
   Stream<List<UiChat>> get chatsStream;
@@ -25,26 +24,18 @@ class ChatsRepoImpl implements ChatsRepo {
   ChatsRepoImpl() {
     _db.initDatabase();
     _accountRepo.accounts.listen(_accountListChanged);
-
-    // Escuta mensagens recebidas globalmente
-    XmppPlugin.instance.onMessageReceived.listen((msg) {
-      _handleIncomingMessage(msg);
-    });
   }
 
   void _accountListChanged(List<UiAccount> accounts) {
     for (final acc in accounts) {
       if (!_accounts.containsKey(acc)) {
         final sub = acc.accountStateStream.listen((state) {
-          if (state is AccountRegistered) {
-            _loadChatsFromDb(acc);
-          }
+          if (state is AccountRegistered) _loadChatsFromDb(acc);
         });
         _accounts[acc] = sub;
       }
     }
-    final toRemove =
-        _accounts.keys.where((a) => !accounts.contains(a)).toList();
+    final toRemove = _accounts.keys.where((a) => !accounts.contains(a)).toList();
     for (final acc in toRemove) {
       _accounts[acc]?.cancel();
       _accounts.remove(acc);
@@ -55,8 +46,7 @@ class ChatsRepoImpl implements ChatsRepo {
     final rows = await _db.getAllDbChatsForAccountId(account.id);
     for (final row in rows) {
       final dbChat = DbChat.fromMap(row);
-      final exists = _chats.any((c) =>
-          c.jid == dbChat.jid && c.account.id == account.id);
+      final exists = _chats.any((c) => c.jid == dbChat.jid && c.account.id == account.id);
       if (!exists) {
         _chats.add(UiChat.fromDbChat(dbChat, account));
       }
@@ -64,24 +54,21 @@ class ChatsRepoImpl implements ChatsRepo {
     _chatsSubject.add(_chats);
   }
 
-  void _handleIncomingMessage(XmppMessage msg) {
-    final fromJid = msg.fromJid ?? '';
+  void addMessage(String fromJid, String toJid, String body) {
     final account = _accounts.keys.firstWhere(
-      (a) => a.id == msg.toJid,
+      (a) => a.id == toJid,
       orElse: () => _accounts.keys.first,
     );
     var chat = _chats.firstWhere(
       (c) => c.jid == fromJid && c.account.id == account.id,
       orElse: () {
         final newChat = UiChat.fromJid(fromJid, account);
-        _db.insert(newChat.getDbChat).then((inserted) {
-          newChat.dbId = inserted.uuid;
-        });
+        _db.insert(newChat.getDbChat).then((inserted) => newChat.dbId = inserted.uuid);
         _chats.add(newChat);
         return newChat;
       },
     );
-    chat.addMessage(msg.body ?? '', fromMe: false);
+    chat.addMessage(body, fromMe: false);
     _chatsSubject.add(_chats);
   }
 }
