f75fc0dc3a52a90ae302c949af1c39b8a067c356
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-06-27 18:48:59 -0300

    Update main_page_state.dart

--- lib/main_page/main_page_state.dart
+++ lib/main_page/main_page_state.dart
@@ -1,36 +1,23 @@
-import 'package:meta/meta.dart';
 import 'package:equatable/equatable.dart';
 import 'package:simple_chat/repo/ui_chat.dart';
 import 'package:simple_chat/roster/roster_repo.dart';
 
 abstract class MainPageState extends Equatable {
-  MainPageState([List props = const []]) : super(props);
-}
-
-@immutable
-class MainPageChatList extends MainPageState {
-
-  final String searchString;
-  final int position;
-  final List<UiChat> activeList;
-
-  MainPageChatList({this.searchString, this.position, @required this
-      .activeList}) : super([searchString, position, activeList]);
-
-      @override
-      String toString() => 'MainPageChatList';
+  const MainPageState();
+  @override
+  List<Object?> get props => [];
 }
 
 class MainPageRosterList extends MainPageState {
-
-  final String searchString;
-  final int position;
   final List<UiBuddy> activeList;
+  const MainPageRosterList({required this.activeList});
+  @override
+  List<Object?> get props => [activeList];
+}
 
-  MainPageRosterList(
-      {this.searchString, this.position, @required this.activeList})
-      : super([searchString, position, activeList]);
-
-      @override
-      String toString() => 'MainPageRosterList';
+class MainPageChatList extends MainPageState {
+  final List<UiChat> activeList;
+  const MainPageChatList({required this.activeList});
+  @override
+  List<Object?> get props => [activeList];
 }
