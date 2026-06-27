a46ba241c7ea3e2521fc01c1c6a55bb3e94988ce
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-06-27 14:20:30 -0300

    Update account_bloc.dart

--- lib/account/account_bloc.dart
+++ lib/account/account_bloc.dart
@@ -1,71 +1,84 @@
 import 'dart:async';
-
-import 'package:simple_chat/account/account.dart';
 import 'package:bloc/bloc.dart';
+import 'package:simple_chat/account/account.dart';
 import 'package:simple_chat/account/account_repo.dart';
 import 'package:simple_chat/service_locator/service_locator.dart';
 import 'package:simple_chat/settings/settings.dart';
 import 'package:xmpp_stone/xmpp_stone.dart' as xmpp;
 
-//We should probably remove this
 class AccountBloc extends Bloc<AccountEvent, AccountState> {
   final settings = sl.get<Settings>();
   final accountRepo = sl.get<AccountRepo>();
 
-  @override
-  AccountState get initialState => AccountUninitialized(account: null);
+  AccountBloc() : super(AccountUninitialized(account: null)) {
+    on<AppStarted>(_onAppStarted);
+    on<Login>(_onLogin);
+    on<Logout>(_onLogout);
+    on<ForgetMe>(_onForgetMe);
+    on<AccountRegisteredEvent>(_onAccountRegistered);
+    on<AccountRegistrationFailedEvent>(_onAccountRegistrationFailed);
+  }
 
-  @override
-  Stream<AccountState> mapEventToState(AccountEvent event) async* {
-    print("account bloc event: ${event.toString()}");
-    if (event is AppStarted) {
-      await settings.isInitialized();
-      bool shouldStartRegistration =
-          settings.getBool(Settings.isAccountSaved) &&
-               settings.getBool(Settings.wasLoggedIn);
-      if (shouldStartRegistration) {
-        var account =  settings.getAccountData();
-        if (account == null) {
-          yield AccountUnregistered(account: null, message: null); // think of
-          // some other event
-        } else {
-          yield AccountRegistering(account:  account);
-          _registerAccount(account);
-        }
+  Future<void> _onAppStarted(
+      AppStarted event, Emitter<AccountState> emit) async {
+    await settings.isInitialized();
+    final shouldStart = settings.getBool(Settings.isAccountSaved) &&
+        settings.getBool(Settings.wasLoggedIn);
+    if (shouldStart) {
+      final account = settings.getAccountData();
+      if (account == null) {
+        emit(AccountUnregistered(account: null, message: null));
+      } else {
+        emit(AccountRegistering(account: account));
+        _registerAccount(account);
       }
-    } else if (event is Login) {
-      xmpp.XmppAccount account = xmpp.XmppAccount(
-          event.username, event.username,
-          event.domain,
-          event.password, event.port);
-      _registerAccount(account);
-
-    } else if (event is Logout) {
-      var account = settings.getAccountData();
-      settings.setBool(Settings.wasLoggedIn, false);
-      if (account != null) {
-        accountRepo.unregister(account);
-      }
-      yield AccountUnregistered(account: account, message: "");
-    } else if (event is ForgetMe) {
-      settings.forgetAccount();
-    } else if (event is AccountRegisteredEvent) {
-      settings.setBool(Settings.wasLoggedIn, true);
-      yield AccountRegistered(account: event.account);
-    } else if (event is AccountRegistrationFailedEvent) {
-      yield AccountUnregistered(account: event.account, message: event.message);
+    } else {
+      emit(AccountUnregistered(account: null, message: null));
     }
   }
 
-  void _registerAccount(xmpp.XmppAccount account){
+  void _onLogin(Login event, Emitter<AccountState> emit) {
+    final account = xmpp.XmppAccount(
+      event.username,
+      event.username,
+      event.domain,
+      event.password,
+      event.port,
+    );
+    emit(AccountRegistering(account: account));
+    _registerAccount(account);
+  }
+
+  void _onLogout(Logout event, Emitter<AccountState> emit) {
+    final account = settings.getAccountData();
+    settings.setBool(Settings.wasLoggedIn, false);
+    if (account != null) accountRepo.unregister(account);
+    emit(AccountUnregistered(account: account, message: ''));
+  }
+
+  void _onForgetMe(ForgetMe event, Emitter<AccountState> emit) {
+    settings.forgetAccount();
+  }
+
+  void _onAccountRegistered(
+      AccountRegisteredEvent event, Emitter<AccountState> emit) {
+    settings.setBool(Settings.wasLoggedIn, true);
+    emit(AccountRegistered(account: event.account));
+  }
+
+  void _onAccountRegistrationFailed(
+      AccountRegistrationFailedEvent event, Emitter<AccountState> emit) {
+    emit(AccountUnregistered(account: event.account, message: event.message));
+  }
+
+  void _registerAccount(xmpp.XmppAccount account) {
     settings.setAccountData(account);
     accountRepo.register(account).accountStateStream.listen((state) {
       if (state is AccountUnregistered) {
-        dispatch(AccountRegistrationFailedEvent(message : state.message));
-      } else if (state is AccountRegistering) {
-
+        add(AccountRegistrationFailedEvent(
+            account: account, message: state.message));
       } else if (state is AccountRegistered) {
-        dispatch(AccountRegisteredEvent());
+        add(AccountRegisteredEvent(account: account));
       }
     });
   }
