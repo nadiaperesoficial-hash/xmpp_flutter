956569f6b18b8d974abfaa1a14b52341e30e25da
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-06-27 18:48:16 -0300

    Update main_page_event.dart

--- lib/main_page/main_page_event.dart
+++ lib/main_page/main_page_event.dart
@@ -1,22 +1,19 @@
 import 'package:equatable/equatable.dart';
-import 'package:meta/meta.dart';
 
 abstract class MainPageEvent extends Equatable {
-  MainPageEvent([List props = const []]) : super(props);
+  const MainPageEvent();
+  @override
+  List<Object?> get props => [];
 }
 
 class MainPageChatListTabActive extends MainPageEvent {
+  const MainPageChatListTabActive();
   @override
-  String toString() {
-    // TODO: implement toString
-    return 'MainPageChatListTabClicked';
-  }
+  String toString() => 'MainPageChatListTabActive';
 }
 
 class MainPageRosterTabActive extends MainPageEvent {
+  const MainPageRosterTabActive();
   @override
-  String toString() {
-    // TODO: implement toString
-    return 'MainPageRosterTabClicked';
-  }
+  String toString() => 'MainPageRosterTabActive';
 }
