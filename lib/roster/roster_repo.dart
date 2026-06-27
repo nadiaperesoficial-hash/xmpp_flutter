83fc268e9298775bdfcb2859f0e10a7d134c1f1f
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-06-27 20:57:50 -0300

    Update roster_repo.dart

--- lib/roster/roster_repo.dart
+++ lib/roster/roster_repo.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:typed_data';
 import 'package:rxdart/rxdart.dart';
 import 'package:simple_chat/account/account_repo.dart';
 import 'package:simple_chat/account/account_state.dart';
@@ -10,24 +11,18 @@ abstract class RosterRepo {
   void close();
 }
 
+class VCardData {
+  final Uint8List? imageData;
+  VCardData({this.imageData});
+}
+
 class UiBuddy {
   final UiAccount account;
   final String jidString;
   final String name;
   VCardData? vCard;
 
-  String get fullJid => jidString;
-
-  UiBuddy({
-    required this.account,
-    required this.jidString,
-    required this.name,
-  });
-}
-
-class VCardData {
-  final List<int>? imageData;
-  VCardData({this.imageData});
+  UiBuddy({required this.account, required this.jidString, required this.name});
 }
 
 class RosterRepoImpl implements RosterRepo {
@@ -35,6 +30,7 @@ class RosterRepoImpl implements RosterRepo {
   final List<UiBuddy> _rosterList = [];
   final _rosterSubject = BehaviorSubject<List<UiBuddy>>();
   final Map<UiAccount, StreamSubscription> _accounts = {};
+  XmppConnection? _connection;
 
   @override
   Stream<List<UiBuddy>> get rosterStream => _rosterSubject.stream;
@@ -47,15 +43,12 @@ class RosterRepoImpl implements RosterRepo {
     for (final acc in accounts) {
       if (!_accounts.containsKey(acc)) {
         final sub = acc.accountStateStream.listen((state) {
-          if (state is AccountRegistered) {
-            _loadRoster(acc);
-          }
+          if (state is AccountRegistered) _loadRoster(acc);
         });
         _accounts[acc] = sub;
       }
     }
-    final toRemove =
-        _accounts.keys.where((a) => !accounts.contains(a)).toList();
+    final toRemove = _accounts.keys.where((a) => !accounts.contains(a)).toList();
     for (final acc in toRemove) {
       _accounts[acc]?.cancel();
       _accounts.remove(acc);
@@ -65,20 +58,29 @@ class RosterRepoImpl implements RosterRepo {
   }
 
   Future<void> _loadRoster(UiAccount acc) async {
-    final contacts = await XmppPlugin.instance.getRoster();
-    for (final contact in contacts) {
-      final exists = _rosterList.any((b) =>
-          b.jidString == contact.jid && b.account.id == acc.id);
-      if (!exists) {
-        final buddy = UiBuddy(
-          account: acc,
-          jidString: contact.jid ?? '',
-          name: contact.name ?? contact.jid ?? '',
-        );
-        _rosterList.add(buddy);
+    try {
+      final params = {
+        'user_jid': acc.id,
+        'password': acc.account.password,
+        'host': acc.account.domain,
+        'port': acc.account.port.toString(),
+        'requireSSLConnection': true,
+        'autoDeliveryReceipt': false,
+        'useStreamManagement': false,
+        'automaticReconnection': false,
+      };
+      _connection = XmppConnection(params);
+      final rosters = await _connection!.getMyRosters() ?? [];
+      for (final r in rosters) {
+        final jid = r['jid']?.toString() ?? '';
+        final name = r['name']?.toString() ?? jid;
+        final exists = _rosterList.any((b) => b.jidString == jid && b.account.id == acc.id);
+        if (!exists) {
+          _rosterList.add(UiBuddy(account: acc, jidString: jid, name: name));
+        }
       }
-    }
-    _rosterSubject.add(_rosterList);
+      _rosterSubject.add(_rosterList);
+    } catch (_) {}
   }
 
   @override
