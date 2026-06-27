691f355d0e063718b35c4230eb962fd494ceb69e
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-06-27 19:50:04 -0300

    Update chats_repo.dart

--- lib/repo/chats_repo.dart
+++ lib/repo/chats_repo.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-import 'dart:core';
-
 import 'package:rxdart/rxdart.dart';
 import 'package:simple_chat/account/account_repo.dart';
 import 'package:simple_chat/account/account_state.dart';
@@ -8,127 +6,82 @@ import 'package:simple_chat/repo/db/db.dart';
 import 'package:simple_chat/repo/db/db_chat.dart';
 import 'package:simple_chat/repo/ui_chat.dart';
 import 'package:simple_chat/service_locator/service_locator.dart';
-import 'package:simple_chat/settings/settings.dart';
-import 'package:xmpp_stone/xmpp_stone.dart' as xmpp;
+import 'package:xmpp_plugin/xmpp_plugin.dart';
 
-//probably will have concurrency issues
+abstract class ChatsRepo {
+  Stream<List<UiChat>> get chatsStream;
+}
+
 class ChatsRepoImpl implements ChatsRepo {
-  final _settings = sl.get<Settings>();
   final _accountRepo = sl.get<AccountRepo>();
-
-  List<UiChat> _chats = List<UiChat>();
-  final _chatsSubject = new BehaviorSubject<List<UiChat>>();
+  final List<UiChat> _chats = [];
+  final _chatsSubject = BehaviorSubject<List<UiChat>>();
+  final Map<UiAccount, StreamSubscription> _accounts = {};
+  final _db = DatabaseHelper();
 
   @override
   Stream<List<UiChat>> get chatsStream => _chatsSubject.stream;
 
-  Map<UiAccount, StreamSubscription> _accounts =
-      Map<UiAccount, StreamSubscription>();
-  Map<UiAccount, xmpp.ChatManager> _chatManagers =
-      Map<UiAccount, xmpp.ChatManager>();
-
-  Map<xmpp.ChatManager, StreamSubscription> _chatManagerStreams =
-      Map<xmpp.ChatManager, StreamSubscription>();
-
-  DatabaseHelper _db = DatabaseHelper();
-
   ChatsRepoImpl() {
     _db.initDatabase();
-    _accountRepo.accounts.listen((accounts) =>
-        //when multiple accounts reimplement this.
-        _accountListChanged(accounts));
-  }
+    _accountRepo.accounts.listen(_accountListChanged);
 
-  _loadChatsFromDb(UiAccount account) {
-    _db.getAllDbChatsForAccountId(account.id).then((accList) {
-      accList.forEach((dbChatMap) {
-        var dbChat = DbChat.fromMap(dbChatMap);
-        //possible conc issue.
-        var existing = _chats.firstWhere(
-            (uiChat) =>
-                dbChat.jid == uiChat.jid.fullJid &&
-                account.id == uiChat.account.id,
-            orElse: () => null);
-        if (existing == null) {
-          var tempChat = UiChat.fromDbChat(dbChat, account);
-          var xmppChat = _chatManagers[account]?.getChat(tempChat.jid);
-          if (xmppChat != null) tempChat.xmppChat = xmppChat;
-          _chats.add(tempChat);
-        }
-      });
+    // Escuta mensagens recebidas globalmente
+    XmppPlugin.instance.onMessageReceived.listen((msg) {
+      _handleIncomingMessage(msg);
     });
   }
 
-  _accountListChanged(List<UiAccount> accounts) {
-    accounts.forEach((acc) {
-      if (!_accounts.keys.contains(acc)) {
-        // ignore: cancel_subscriptions
-        var sub = acc.accountStateStream.listen((state) {
+  void _accountListChanged(List<UiAccount> accounts) {
+    for (final acc in accounts) {
+      if (!_accounts.containsKey(acc)) {
+        final sub = acc.accountStateStream.listen((state) {
           if (state is AccountRegistered) {
-            _addChatManager(acc);
             _loadChatsFromDb(acc);
           }
         });
         _accounts[acc] = sub;
       }
-    });
-    List<UiAccount> toRemove = List<UiAccount>();
-    _accounts.keys.forEach((oldAcc) {
-      if (!accounts.contains(oldAcc)) {
-        toRemove.add(oldAcc);
-      }
-    });
-    toRemove.forEach((acc) {
-      _accounts[acc].cancel();
+    }
+    final toRemove =
+        _accounts.keys.where((a) => !accounts.contains(a)).toList();
+    for (final acc in toRemove) {
+      _accounts[acc]?.cancel();
       _accounts.remove(acc);
-      _removeChatManager(acc);
-    });
-  }
-
-  _addChatManager(UiAccount acc) {
-    if (!_chatManagers.containsKey(acc)) {
-      xmpp.Connection connection = xmpp.Connection.getInstance(acc.account);
-      xmpp.ChatManager manager = xmpp.ChatManager.getInstance(connection);
-      _chatManagers[acc] = manager;
-      if (manager.chats.isNotEmpty) {
-        _chatListChangedForManager(acc, manager, manager.chats);
-      }
-      manager.chatListStream.listen((chats) {
-        _chatListChangedForManager(acc, manager, chats);
-      });
     }
   }
 
-  _removeChatManager(UiAccount acc) {
-    _chatManagers.remove(acc);
-    //remove chats from manager
-  }
-
-  void _chatListChangedForManager(
-      UiAccount account, xmpp.ChatManager manager, List<xmpp.Chat> chats) {
-    chats.forEach((xmppChat) {
-      //var tempChat = UiChat.fromXmppChat(xmppChat, account);
-      var existing = _chats.firstWhere(
-          (uichat) =>
-              xmppChat.jid == uichat.jid && account.id == uichat.account.id,
-          orElse: () => null);
-      if (existing == null) {
-        var tempChat = UiChat.fromXmppChat(xmppChat, account);
-        _db.insert(tempChat.getDbChat).then((insertedChat) {
-          tempChat.dbId = insertedChat.uuid;
-          _chats.add(tempChat);
-          _chatsSubject.add(_chats);
-        });
-      } else {
-        if (existing.xmppChat == null) {
-          existing.xmppChat = xmppChat;
-        }
+  Future<void> _loadChatsFromDb(UiAccount account) async {
+    final rows = await _db.getAllDbChatsForAccountId(account.id);
+    for (final row in rows) {
+      final dbChat = DbChat.fromMap(row);
+      final exists = _chats.any((c) =>
+          c.jid == dbChat.jid && c.account.id == account.id);
+      if (!exists) {
+        _chats.add(UiChat.fromDbChat(dbChat, account));
       }
-    });
+    }
+    _chatsSubject.add(_chats);
   }
-}
 
-abstract class ChatsRepo {
-  @override
-  Stream<List<UiChat>> get chatsStream;
+  void _handleIncomingMessage(XmppMessage msg) {
+    final fromJid = msg.fromJid ?? '';
+    final account = _accounts.keys.firstWhere(
+      (a) => a.id == msg.toJid,
+      orElse: () => _accounts.keys.first,
+    );
+    var chat = _chats.firstWhere(
+      (c) => c.jid == fromJid && c.account.id == account.id,
+      orElse: () {
+        final newChat = UiChat.fromJid(fromJid, account);
+        _db.insert(newChat.getDbChat).then((inserted) {
+          newChat.dbId = inserted.uuid;
+        });
+        _chats.add(newChat);
+        return newChat;
+      },
+    );
+    chat.addMessage(msg.body ?? '', fromMe: false);
+    _chatsSubject.add(_chats);
+  }
 }
