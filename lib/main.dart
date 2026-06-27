6a5893839501298425fa6cf5535f9a00663c1110
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-06-27 14:19:47 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,159 +1,66 @@
-// Copyright 2017, the Chromium project authors.  Please see the AUTHORS file
-// for details. All rights reserved. Use of this source code is governed by a
-// BSD-style license that can be found in the LICENSE file.
-
-import 'package:bloc/bloc.dart' as bloc;
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:simple_chat/account/account.dart';
-import 'package:simple_chat/chat_list.dart';
-import 'package:xmpp_stone/xmpp_stone.dart' as xmpp;
-import 'package:simple_chat/const.dart';
-import 'package:simple_chat/service_locator/service_locator.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:simple_chat/account/account.dart';
 import 'package:simple_chat/login/login_page.dart';
-
-import 'main_page/main_page_widget.dart';
+import 'package:simple_chat/main_page/main_page_widget.dart';
+import 'package:simple_chat/service_locator/service_locator.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   setupServiceLocator();
-  bloc
-      .BlocSupervisor()
-      .delegate = SimpleBlocDelegate();
   runApp(MyApp());
 }
 
-class SimpleBlocDelegate extends bloc.BlocDelegate {
-  @override
-  void onTransition(bloc.Transition transition) {
-    print(transition);
-  }
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  void onError(Object error, StackTrace stacktrace) {
-    print(error);
-  }
+  State<MyApp> createState() => _AppState();
 }
 
 class _AppState extends State<MyApp> {
-  AccountBloc accountBloc;
-  final GlobalKey<NavigatorState> navigatorKey = GlobalKey(debugLabel: "Main Navigator");
+  late AccountBloc accountBloc;
+  final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey(debugLabel: 'Main Navigator');
 
   @override
   void initState() {
-    accountBloc = AccountBloc();
-    accountBloc.dispatch(AppStarted());
     super.initState();
+    accountBloc = AccountBloc();
+    accountBloc.add(AppStarted());
   }
 
   @override
   void dispose() {
-    accountBloc.dispose();
+    accountBloc.close();
     super.dispose();
   }
 
-  Route _getGeneratedRoute(RouteSettings routeSettings) {
-    final List<String> path = routeSettings.name.split('/');
-    if (path[0] == '') return null;
-//    if (path[0] == 'login') {
-//      return new MaterialPageRoute(
-//        builder: (BuildContext context) => LoginPage(),
-//        settings: routeSettings,
-//      );
-//    }
-//    if (path[0] == Chats.tag && path[1] != null && path[1].isNotEmpty) {
-//      return new MaterialPageRoute(
-//        builder: (BuildContext context) => ChatList(),
-//        settings: routeSettings,
-//      );
-//    }
-  }
-
-  //  @override
-  Widget build(BuildContext context) {
-    accountBloc.state.listen((state)  {
-      if (state is AccountUninitialized) {
-        navigatorKey.currentState.pushNamed(LoginPage.tag);
-      }
-      if (state is AccountRegistered) {
-        navigatorKey.currentState.pushNamed(MainPage.TAG);
-      }
-      if (state is AccountUnregistered) {
-        navigatorKey.currentState.pushNamed(LoginPage.tag);
-      }
-      if (state is AccountRegistering) {
-        navigatorKey.currentState.popUntil(ModalRoute.withName(LoginPage.tag));
-        navigatorKey.currentState.pushNamed(LoginPage.tag);
-      }
-
-    });
-    final routes = <String, WidgetBuilder>{
-      LoginPage.tag: (context) => LoginPage(accountBloc),
-      MainPage.TAG: (context) => MainPage(accountBloc)
-    };
-
-
-
-    return MaterialApp(
-      navigatorKey: navigatorKey,
-      initialRoute: LoginPage.tag,
-      debugShowCheckedModeBanner: false,
-
-      home: LoginPage(accountBloc),
-      routes: routes,
-      onGenerateRoute: _getGeneratedRoute,
-
-    );
-//    return BlocProvider<AccountBloc>(
-//      bloc: _accountBloc,
-//      child: MaterialApp(
-//        home: BlocBuilder<AccountEvent, AccountState>(
-//          bloc: _accountBloc,
-//          builder: (BuildContext context, AccountState state) {
-//            if (state is AccountUninitialized) {
-//              return LoginPage();
-//            }
-//            if (state is AccountRegistered) {
-//              return MainPage();
-//            }
-//            if (state is AccountUnregistered) {
-//              return LoginPage();
-//            }
-//            if (state is AccountRegistering) {
-//              return LoginPage();
-//            }
-//          },
-//        ),
-//      ),
-//    );
-  }
-}
-
-class MatApp extends MaterialApp {
-
-}
-
-class MyApp extends StatefulWidget {
-
-  MyApp({Key key}) : super(key: key);
-
   @override
-  State<MyApp> createState() => _AppState();
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      title: 'Kodeversitas',
-//      debugShowCheckedModeBanner: false,
-//      theme: ThemeData(
-//        primarySwatch: Colors.lightBlue,
-//        fontFamily: 'Nunito',
-//      ),
-//      home: LoginPage(),
-//      routes: routes,
-//      onGenerateRoute: _getGeneratedRoute,
-//    );
-//  }
-
-
+  Widget build(BuildContext context) {
+    return BlocListener<AccountBloc, AccountState>(
+      bloc: accountBloc,
+      listener: (context, state) {
+        if (state is AccountUninitialized || state is AccountUnregistered) {
+          navigatorKey.currentState
+              ?.pushNamedAndRemoveUntil(LoginPage.tag, (_) => false);
+        } else if (state is AccountRegistered) {
+          navigatorKey.currentState
+              ?.pushNamedAndRemoveUntil(MainPage.TAG, (_) => false);
+        } else if (state is AccountRegistering) {
+          navigatorKey.currentState
+              ?.pushNamedAndRemoveUntil(LoginPage.tag, (_) => false);
+        }
+      },
+      child: MaterialApp(
+        navigatorKey: navigatorKey,
+        debugShowCheckedModeBanner: false,
+        initialRoute: LoginPage.tag,
+        routes: {
+          LoginPage.tag: (_) => LoginPage(accountBloc),
+          MainPage.TAG: (_) => MainPage(accountBloc),
+        },
+      ),
+    );
+  }
 }
