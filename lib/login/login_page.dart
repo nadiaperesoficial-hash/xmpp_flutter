2d8866b6070f901807564f50bd233c8d2067e3e6
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-06-27 14:39:42 -0300

    Update login_page.dart

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -1,50 +1,42 @@
-import 'dart:async';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:simple_chat/account/account_bloc.dart';
-import 'package:simple_chat/const.dart';
-import 'package:simple_chat/chat_list.dart';
-import 'package:simple_chat/login/login.dart';
-import 'package:xmpp_stone/xmpp_stone.dart' as xmpp;
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:simple_chat/login/login_bloc.dart';
+import 'package:simple_chat/login/login_form.dart';
 
 class LoginPage extends StatefulWidget {
-  static String tag = 'login';
-  AccountBloc accountBloc;
-  LoginPage(this.accountBloc);
+  static const String tag = 'login';
+  final AccountBloc accountBloc;
+
+  const LoginPage(this.accountBloc, {Key? key}) : super(key: key);
 
   @override
-  _LoginPageState createState() => new _LoginPageState();
+  State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-  LoginBloc _loginBloc ;
+  late LoginBloc _loginBloc;
 
   @override
   void initState() {
-    print("init State");
-    _loginBloc = LoginBloc(
-      accountBloc: widget.accountBloc,
-    );
     super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    print("build");
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: LoginForm(loginBloc: _loginBloc),
-      ),
-    );
+    _loginBloc = LoginBloc(accountBloc: widget.accountBloc);
   }
 
   @override
   void dispose() {
-    _loginBloc.dispose();
+    _loginBloc.close();
     super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: LoginForm(loginBloc: _loginBloc),
+        ),
+      ),
+    );
   }
 }
