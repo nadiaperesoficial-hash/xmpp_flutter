ffb01ab92fd9e8e2f5c4e2cd759c2f907178953d
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-06-28 07:21:23 -0300

    Update service_locator.dart

--- lib/service_locator/service_locator.dart
+++ lib/service_locator/service_locator.dart
@@ -1,10 +1,10 @@
 import 'package:get_it/get_it.dart';
 import 'package:simple_chat/account/account_repo.dart';
+import 'package:simple_chat/repo/chats_repo.dart';
 import 'package:simple_chat/roster/roster_repo.dart';
 import 'package:simple_chat/settings/settings.dart';
-import 'package:simple_chat/repo/chats_repo.dart';
 
-GetIt sl = new GetIt();
+GetIt sl = GetIt.instance;
 
 void setupServiceLocator() {
   sl.registerSingleton<Settings>(SettingsImpl());
