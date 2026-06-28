129806b444f040e208c4b45c7477f2db9174cda2
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-06-28 11:42:10 -0300

    Update login_event.dart

--- lib/login/login_event.dart
+++ lib/login/login_event.dart
@@ -1,19 +1,19 @@
-import 'package:equatable/equatable.dart';
+part of 'login_bloc.dart';
 
-abstract class LoginEvent extends Equatable {
-  const LoginEvent();
-  @override
-  List<Object?> get props => [];
-}
+abstract class LoginEvent {}
 
 class LoginButtonPressed extends LoginEvent {
   final String username;
   final String password;
   final String domain;
   final int port;
-  const LoginButtonPressed({required this.username, required this.password, required this.domain, required this.port});
-  @override
-  List<Object?> get props => [username, password, domain, port];
+
+  const LoginButtonPressed({
+    required this.username,
+    required this.password,
+    required this.domain,
+    required this.port,
+  });
 }
 
 class RegisterButtonPressed extends LoginEvent {
@@ -21,18 +21,20 @@ class RegisterButtonPressed extends LoginEvent {
   final String password;
   final String domain;
   final int port;
-  const RegisterButtonPressed({required this.username, required this.password, required this.domain, required this.port});
-  @override
-  List<Object?> get props => [username, password, domain, port];
+
+  const RegisterButtonPressed({
+    required this.username,
+    required this.password,
+    required this.domain,
+    required this.port,
+  });
 }
 
-class ExtendPressed extends LoginEvent { const ExtendPressed(); }
+class ExtendPressed extends LoginEvent {}
 
 class RememberMePressed extends LoginEvent {
   final bool rememberMeValue;
   const RememberMePressed({required this.rememberMeValue});
-  @override
-  List<Object?> get props => [rememberMeValue];
 }
 
 class LoginDataLoadedEvent extends LoginEvent {
@@ -42,16 +44,23 @@ class LoginDataLoadedEvent extends LoginEvent {
   final int port;
   final bool wasExtended;
   final bool rememberMe;
-  const LoginDataLoadedEvent({required this.username, required this.password, required this.domain, required this.port, required this.wasExtended, required this.rememberMe});
-  @override
-  List<Object?> get props => [username, password, domain, port, wasExtended, rememberMe];
+
+  const LoginDataLoadedEvent({
+    required this.username,
+    required this.password,
+    required this.domain,
+    required this.port,
+    required this.wasExtended,
+    required this.rememberMe,
+  });
 }
 
-class LoginDataShownEvent extends LoginEvent { const LoginDataShownEvent(); }
+class LoginDataShownEvent extends LoginEvent {}
 
 class LoginFailureEvent extends LoginEvent {
-  final String? message;
-  const LoginFailureEvent({this.message});
-  @override
-  List<Object?> get props => [message];
+  final String message;
+  const LoginFailureEvent({required this.message});
 }
+
+// Novo evento para sucesso de login
+class LoginSuccessEvent extends LoginEvent {}
